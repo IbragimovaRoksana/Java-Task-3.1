e3181d5557b2c2c0368a79ce090b4e08792538e2
IbragimovaRoksana <roksalana91@mail.ru>
2020-11-24 19:57:00 +0500

    added main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        long ticket_price;
+        long free_miles;
+        ticket_price=3000_00;
+        free_miles = ticket_price/20/100;
+        System.out.println(free_miles);
+    }
+}
