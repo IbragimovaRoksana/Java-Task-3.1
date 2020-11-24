698101d08a171777888012f33e3a5b276b749a09
IbragimovaRoksana <roksalana91@mail.ru>
2020-11-24 21:39:24 +0500

    Corrected programm's text

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        long ticket_price;
-        long free_miles;
-        ticket_price=3000_00;
-        free_miles = ticket_price/20/100;
+        long ticket_price = 3000_00;
+        long free_miles = ticket_price/20/100;
         System.out.println(free_miles);
     }
 }
